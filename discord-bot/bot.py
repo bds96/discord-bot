540387f948ae37d4b5588dbdff3381427694b89c
bds <14mbdsks@gmail.com>
2020-09-23 13:49:23 -0700

    Randomize video

--- discord-bot/bot.py
+++ discord-bot/bot.py
@@ -13,6 +13,12 @@ target_channel_id = 724773710996832311
 
 cache = set()
 
+videos = [
+	"https://www.youtube.com/watch?v=apANX0_qlr8",
+	"https://www.youtube.com/watch?v=7C0D_hL8lEc",
+	"https://www.youtube.com/watch?v=x5D2x7OHq5k"
+]
+
 @tasks.loop(hours=1)
 async def called_once_a_day():
 	today = datetime.today()
@@ -21,7 +27,7 @@ async def called_once_a_day():
 		if today.weekday() == 2: # Wednesday 
 		    message_channel = bot.get_channel(target_channel_id)
 		    print(f"Got channel {message_channel}")
-		    await message_channel.send("https://www.youtube.com/watch?v=apANX0_qlr8")
+		    await message_channel.send(videos[random.randint(0,2)])
 	else
 		cache.add(ymd)
 
