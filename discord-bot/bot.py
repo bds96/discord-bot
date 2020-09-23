611ae71f907c2bdc8db9cb4122edd5a17efeab9e
bds <14mbdsks@gmail.com>
2020-09-23 13:44:14 -0700

    Check each hour instead of every 24 hours

--- discord-bot/bot.py
+++ discord-bot/bot.py
@@ -11,12 +11,19 @@ bot = commands.Bot("!")
 
 target_channel_id = 724773710996832311
 
-@tasks.loop(hours=24)
+cache = set()
+
+@tasks.loop(hours=1)
 async def called_once_a_day():
-	if datetime.today().weekday() == 2: # Wednesday 
-	    message_channel = bot.get_channel(target_channel_id)
-	    print(f"Got channel {message_channel}")
-	    await message_channel.send("https://www.youtube.com/watch?v=apANX0_qlr8")
+	today = datetime.today()
+	ymd = (today.year, today.month, today.day)
+	if ymd not in cache:
+		if today.weekday() == 2: # Wednesday 
+		    message_channel = bot.get_channel(target_channel_id)
+		    print(f"Got channel {message_channel}")
+		    await message_channel.send("https://www.youtube.com/watch?v=apANX0_qlr8")
+	else
+		cache.add(ymd)
 
 @called_once_a_day.before_loop
 async def before():
